ee8ca2dcb4c4e0cd543afc3f75cf80d161c66d55
Simarpreet Singh <simar@linux.com>
2015-05-11 23:34:58 -0400

    adding psuedo-code
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- memory.v
+++ memory.v
@@ -1,7 +1,8 @@
 // ECE 429
-module pd1_memory(clock, data_in, access_size, rw, busy, data_output);
+module memory(clock, address, data_in, access_size, rw, busy, data_output);
 
 input clock;
+input address[31:0];
 input data_in[31:0];
 input access_size[1:0];
 input rw;
@@ -10,3 +11,26 @@ output data_out[31:0];
 
 parameter depth = 1000000;
 
+/* 
+   // loop
+   {
+     if rw == 1:
+	asrt busy -> 1
+	// do read logic 
+	// we get address
+ 	if address == 32 bits
+		if access_size > 32 bits
+			cycles - each 4 bytes wide
+		else
+			send 4 bytes of data on data bus
+     else // rw = 0
+	asrt busy -> 1
+	// do write logic
+	// we get address and data_in
+
+     // at the end of the loop de-assrt busy
+     asrt busy -> 0
+   }
+*/
+
+	
