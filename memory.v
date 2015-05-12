f76338b54d06388aa62bb3501f814476c82daf63
parallels <raunaq.sawhney19@gmail.com>
2015-05-11 23:12:47 -0400

    add part 1 and 2 code

--- memory.v
+++ memory.v
@@ -1,1 +1,12 @@
 // ECE 429
+module pd1_memory(clock, data_in, access_size, rw, busy, data_output);
+
+input clock;
+input data_in[31:0];
+input access_size[1:0];
+input rw;
+input busy;
+output data_out[31:0];
+
+parameter depth = 1000000;
+
