ce2e189b4c60bbc24a172da0d6e5b05611c6c4bb
Simarpreet Singh <s244sing@uwaterloo.ca>
2015-05-14 17:13:18 -0400

    memory: add logic for global counters and cyc_ctr
    Signed-off-by: Simarpreet Singh <s244sing@uwaterloo.ca>

--- memory.v
+++ memory.v
@@ -26,7 +26,8 @@ output data_out[data_width-1:0];
 reg [7:0] mem[depth]; // should be [7:0] since its byte addressible memory
 reg [7:0] data;
 reg [7:0] byte[3:0];
-
+reg [31:0] global_cur_addr;
+integer cyc_ctr = 0;
 always @(posedge clock)
 begin : WRITE
 	// rw = 1
@@ -48,6 +49,12 @@ end
 assign data_out = {byte[0], byte[1], byte[2], byte[3]};
 
 always @(posedge clock)
+    if (!busy) begin
+        global_cur_add <= address;
+    end
+end
+
+always @(posedge clock)
 begin : READ
 	if (!rw && !busy && enable) begin
 		busy = 1; 
@@ -57,19 +64,22 @@ begin : READ
 			for (i = 0; i < 4; i = i+1) begin
 				byte[i] <= mem[address+i];
 			end
+        // 01: 4 words
 		end else if (access_size == 2'b0_1) begin
 			for (i = 0; i < 4 && cyc_ctr < 4; i = i+1) begin
 				byte[i] <= mem[global_cur_add+i];
-		
+	        end
+        // 10: 8 words
 		end else if (access_size == 2'b1_0) begin
 			for (i = 0; i < 4 && cyc_ctr < 8; i = i+1) begin
 				byte[i] <= mem[global_cur_add+i];
-  		
+  	        end
+        // 11: 16 words
 		end else if (access_size == 2'b1_1) begin
 			for (i = 0; i < 4 && cyc_ctr < 16; i = i+1) begin
 				byte[i] <= mem[global_cur_add+i];
-
-		end 
+		    end
+        end 
         global_cur_addr = global_cur_addr + 4;
         cyc_ctr = cyc_ctr + 1;
 	end
