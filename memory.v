1b2990d87126877b787d88e1bf8da06ae0c7407b
Raunaq <raunaq.sawhney19@gmail.com>
2015-05-14 23:04:43 -0400

    fix bugs from compilation. changed busy from input to output
    Signed-off-by: Raunaq <raunaq.sawhney19@gmail.com>

--- memory.v
+++ memory.v
@@ -1,5 +1,5 @@
 // ECE 429
-module memory(clock, address, data_in, access_size, rw, busy, enable, data_output);
+module memory(clock, address, data_in, access_size, rw, busy, enable, data_out);
 
 parameter data_width = 32;
 parameter address_width = 32;
@@ -12,30 +12,32 @@ parameter BYTE = 8;
 
 // Input Ports
 input clock;
-input address[address_width-1:0];
-input data_in[data_width-1:0];
-input access_size[1:0];
+input [address_width-1:0] address;
+input [data_width-1:0] data_in;
+input [1:0] access_size;
 input rw;
-input busy;
 input enable;
 
 // Output Ports
-output data_out[data_width-1:0];
+output busy;
+output [data_width-1:0] data_out;
 
 // Create a 1MB deep memory of 8-bits (1 byte) width
-reg [7:0] mem[depth]; // should be [7:0] since its byte addressible memory
+reg [7:0] mem[0:depth]; // should be [7:0] since its byte addressible memory
 reg [7:0] data;
 reg [7:0] byte[3:0];
 reg [31:0] global_cur_addr;
 integer cyc_ctr = 0;
+integer i = 0;
+
 always @(posedge clock)
 begin : WRITE
 	// rw = 1
 	if (rw && !busy && enable) begin
-		busy = 1;
+		assign busy = 1;
 		mem[address] <= data_in;
 	end
-	busy = 0;
+	assign busy = 0;
 end
 
 /*
@@ -50,9 +52,8 @@ assign data_out = {byte[0], byte[1], byte[2], byte[3]};
 
 always @(posedge clock)
     if (!busy) begin
-        global_cur_add <= address;
+        global_cur_addr <= address;
     end
-end
 
 always @(posedge clock)
 begin : READ
@@ -67,17 +68,17 @@ begin : READ
         // 01: 4 words
 		end else if (access_size == 2'b0_1) begin
 			for (i = 0; i < 4 && cyc_ctr < 4; i = i+1) begin
-				byte[i] <= mem[global_cur_add+i];
+				byte[i] <= mem[global_cur_addr+i];
 	        end
         // 10: 8 words
 		end else if (access_size == 2'b1_0) begin
 			for (i = 0; i < 4 && cyc_ctr < 8; i = i+1) begin
-				byte[i] <= mem[global_cur_add+i];
+				byte[i] <= mem[global_cur_addr+i];
   	        end
         // 11: 16 words
 		end else if (access_size == 2'b1_1) begin
 			for (i = 0; i < 4 && cyc_ctr < 16; i = i+1) begin
-				byte[i] <= mem[global_cur_add+i];
+				byte[i] <= mem[global_cur_addr+i];
 		    end
         end 
         global_cur_addr = global_cur_addr + 4;
