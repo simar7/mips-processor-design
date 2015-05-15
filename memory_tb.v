90137a0ccfa5fffc519c16edc760f9fbfcff685d
Raunaq <raunaq.sawhney19@gmail.com>
2015-05-15 15:01:01 -0400

    fix: reading file

--- memory_tb.v
+++ memory_tb.v
@@ -23,6 +23,7 @@ wire [data_width-1:0] data_out;
 
 // fileIO stuff
 integer fd;
+integer scan_fd;
 integer status_read, status_write;
 integer sscanf_ret;
 reg [31:0] fd_in;
@@ -41,32 +42,32 @@ memory M0 (
 );
 
 initial begin
-	
+
+	// FIXME: WTF (read: -2147483641) is fd when doing fopen?!
+	// FIXME: Find the right location where SumArray.x resides.
+	fd = $fopen("SumArray.x", "r");
+	if (!fd)
+		$display("Could not open");
+
 	clock = 0;
 	address = 0;
 	data_in = 0;
 	access_size = 0;
 	rw = 0;
 	enable = 1;
-
-	// FIXME: WTF (read: -2147483641) is fd when doing fopen?!
-	// FIXME: Find the right location where SumArray.x resides.
-	fd = $fopen("/home/simar/SumArray.x", "r");
-	if (!fd)
-		$display("Could not open");
 	
 	// Read the data from file, stored in local regs.
 	while (!$feof(fd)) begin
-	$display("fd_in = %x", fd_in);
+		scan_fd = $fscanf(fd, "%x", fd_in);
+		$display("fd_in = %x", fd_in);
 		@(posedge clock);
 			status_read = $fscanf(fd, "%h\n", fd_in[31:16], fd_in[15:0]);
 			sscanf_ret = $sscanf(str, "%x", fd_in);
 	end
 	$fclose(fd);
-
 end
 
 always
-	clock = !clock;
+	#1 clock = ! clock;
 
-endmodule
+endmodule 
