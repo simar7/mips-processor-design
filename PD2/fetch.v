8000e69a32f75fcdc590da552904aa0585f21299
Simarpreet Singh <simar@linux.com>
2015-05-24 02:12:04 -0400

    someone tell rsawhney how to write compilable code
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- PD2/fetch.v
+++ PD2/fetch.v
@@ -1,23 +1,18 @@
-module fetch (clock, pc_out, rw_out, stall_input, access_size_out);
+module fetch (clock, pc, rw, stall, access_size, enable);
 
 // Input Ports
 input clock;
-input stall_input;
-
-input rw;
+input stall;
 
 // Output Ports
-output [31:0] pc_out;
-output rw_out;
-output [31:0] access_size_out;
-output enable_out;			// Goes to enable signal of the memory
+output reg [31:0] pc;
+output reg rw;
+output reg [31:0] access_size;
+output reg enable;			// Goes to enable signal of the memory
 
 // Parameters
 parameter start_addr = 32'h80020000;
-parameter word_size = 2'b11;		// 11 = 4 bytes = 1 word
-
-// Registers
-reg [31:0] pc;
+parameter word_size = 4;
 
 initial begin
 	pc = start_addr;
@@ -26,26 +21,24 @@ end
 always @(posedge clock)
 begin: FETCH
 
-	if (rw && !stall_input) begin
+	if (rw && !stall) begin
 
 		pc = pc + 4;
 		
-		pc_out 		<= pc;
-		rw_out 		<= 1;		// Always set to 1 for Fetch Stage
-		access_size_out <= word_size	// Always set to 1 word for Fetch Stage
-		enable_out	<= 1;
+		pc 		<= pc;
+		rw 		<= 1;		// Always set to 1 for Fetch Stage
+		access_size 	<= word_size;	// Always set to 1 word for Fetch Stage
+		enable	<= 1;
 
 
-	end else if (rw && stall_input) begin
+	end else if (rw && stall) begin
 	
-		pc_out 		<= pc;
-		rw_out		<= 1;		// Always set to 1 for Fetch Stage
-		access_size_out	<= word_size	// Always set to 1 word for Fetch Stage
-		enable_out	<= 0;
+		pc 		<= pc;
+		rw		<= 1;		// Always set to 1 for Fetch Stage
+		access_size	<= word_size;	// Always set to 1 word for Fetch Stage
+		enable	<= 0;
 
 	end
 end
-	
-		
-	
 
+endmodule
