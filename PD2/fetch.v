4ccc173a969eafe21decb3076a881ea5196d233b
Raunaq Sawhney <raunaq.sawhney19@gmail.com>
2015-05-25 19:01:45 -0400

    FIX: pc_reg is a register, pc is output
    Signed-off-by: Raunaq Sawhney <raunaq.sawhney19@gmail.com>

--- PD2/fetch.v
+++ PD2/fetch.v
@@ -14,6 +14,9 @@ output reg enable;			// Goes to enable signal of the memory
 parameter start_addr = 32'h80020000;
 parameter word_size = 4;
 
+// Registers
+reg [31:0] pc_reg;
+
 initial begin
 	pc = start_addr;
 end
@@ -23,20 +26,20 @@ begin: FETCH
 
 	if (rw && !stall) begin
 
-		pc = pc + 4;
+		pc_reg	 	= pc_reg + 4;
 		
-		pc 		<= pc;
+		pc 		<= pc_reg;
 		rw 		<= 1;		// Always set to 1 for Fetch Stage
 		access_size 	<= word_size;	// Always set to 1 word for Fetch Stage
-		enable	<= 1;
+		enable		<= 1;
 
 
 	end else if (rw && stall) begin
 	
-		pc 		<= pc;
+		pc 		<= pc_reg;
 		rw		<= 1;		// Always set to 1 for Fetch Stage
 		access_size	<= word_size;	// Always set to 1 word for Fetch Stage
-		enable	<= 0;
+		enable		<= 0;
 
 	end
 end
