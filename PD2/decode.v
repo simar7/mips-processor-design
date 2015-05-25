06e79d97228a80262f379347802126363b63d0b8
Simarpreet Singh <simar@linux.com>
2015-05-25 19:47:29 -0400

    add insn classifications
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- PD2/decode.v
+++ PD2/decode.v
@@ -8,6 +8,31 @@ input [31:0] pc;
 // Registers
 reg [31:0] pc_reg;
 
+
+/*
+  Instructions to support
+ 
+1) add, addiu, addu, sub, subu
+2) mult, multu, div, divu, mfhi, mflo
+3) slt, slti, sltu, sltiu
+4) sll, sllv, srl, srlv, sra, srav, and, or, ori, xor, xori, nor
+5) lw, sw, li (ori, lui), lb, sb, lbu, move
+6) j, jal, jalr, jr, beq, beqz, bne, bnez, bgez, bgtz, blez, bltz
+7) nop
+
+R-type insn: 
+add, sub, addu, subu, mult, multu, div, divu, mfhi,
+mflo, slt, sltu, sll, sllv, srl, srlv, sra, srav,
+and, or, xor, nor, jalr, jr, nop
+
+I-type insn:
+addiu, slti, slti, sltiu, ori, xori
+
+J-type insn:
+j, jal, beq, beqz, bne, bnez, bgez, bgtz, blez, bltz
+
+*/
+
 always @(posedge clock)
 begin : DECODE
 
