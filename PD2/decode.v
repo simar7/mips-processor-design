d4f714f7fce799ba7939cfd92b902d00aa854a57
Simarpreet Singh <simar@linux.com>
2015-05-25 19:16:45 -0400

    adding a sample tb and some stuff in decode.v
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- PD2/decode.v
+++ PD2/decode.v
@@ -1,3 +1,16 @@
 module decode(clock, insn, pc);
 
+// Input ports
+input clock;
+input [31:0] insn;
+input [31:0] pc;
+
+// Registers
+reg [31:0] pc_reg;
+
+always @(posedge clock)
+begin : DECODE
+
+	
+
 endmodule
