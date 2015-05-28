69638d3aadb9f95500e29a58aa59047135e6cf15
Raunaq Sawhney <raunaq.sawhney19@gmail.com>
2015-05-28 01:10:12 -0400

    ADD: new R-type decode cases
    Signed-off-by: Raunaq Sawhney <raunaq.sawhney19@gmail.com>

--- PD2/decode.v
+++ PD2/decode.v
@@ -20,47 +20,44 @@ output reg [5:0] func_out;
 //output reg [15:0] imm_out;
 //output reg [25:0] target_out;
 
-parameter ADD 	= 100000; //ADD;
+parameter ADD 	= 6'b100000; //ADD;
 parameter ADDU 	= 6'b100001; //ADDU;
-parameter SUB	= 100010; //SUB;
-parameter SUBU	= 100011; //SUBU;
-parameter MULT	= 011000; //MULT;
-parameter MULTU = 011001; //MULTU;
-parameter DIV	= 011010; //DIV;
-parameter DIVU 	= 011011; //DIVU;
-parameter MFHI	= 010000; //MFHI;
-parameter MFLO 	= 010010; //MFLO;
-parameter SLT	= 101010; //SLT;
-parameter SLTU	= 101011; //SLTU;
-parameter SLL	= 000000; //SLL;
-parameter SLLV	= 000100; //SLLV;
-parameter SRL	= 000010; //SRL;
-parameter SRLV	= 000110; //SRLV;
-parameter SRA	= 000011; //SRA;
-parameter SRAV	= 000111; //SRAV;
-parameter AND	= 100100; //AND;
-parameter OR	= 100101; //OR;
-parameter XOR	= 100110; //XOR;
-parameter NOR	= 100111; //NOR
-parameter JALR	= 001001; //JALR;
-parameter JR	= 001000; //JR;
+parameter SUB	= 6'b100010; //SUB;
+parameter SUBU	= 6'b100011; //SUBU;
+parameter MULT	= 6'b011000; //MULT;
+parameter MULTU = 6'b011001; //MULTU;
+parameter DIV	= 6'b011010; //DIV;
+parameter DIVU 	= 6'b011011; //DIVU;
+parameter MFHI	= 6'b010000; //MFHI;
+parameter MFLO 	= 6'b010010; //MFLO;
+parameter SLT	= 6'b101010; //SLT;
+parameter SLTU	= 6'b101011; //SLTU;
+parameter SLL	= 6'b000000; //SLL;
+parameter SLLV	= 6'b000100; //SLLV;
+parameter SRL	= 6'b000010; //SRL;
+parameter SRLV	= 6'b000110; //SRLV;
+parameter SRA	= 6'b000011; //SRA;
+parameter SRAV	= 6'b000111; //SRAV;
+parameter AND	= 6'b100100; //AND;
+parameter OR	= 6'b100101; //OR;
+parameter XOR	= 6'b100110; //XOR;
+parameter NOR	= 6'b100111; //NOR
+parameter JALR	= 6'b001001; //JALR;
+parameter JR	= 6'b001000; //JR;
 
-/*
-
-			6'b001001: //ADDIU
-			6'b001010: //SLTI
-			6'b001011: //SLTIU
-			6'b001101: //ORI
-			6'b001110: //XORI
-			6'b100011: //LW
-			6'b101011: //SW
-			6'b100000: //LB
-			6'b101000: //SB
-			6'b100100: //LBU
-			6'b000100: //BEQ
-			6'b000101: //BNE
-			6'b000111: //BGTZ
-*/
+parameter ADDIU = 6'b001001; //ADDIU
+parameter SLTI  = 6'b001010; //SLTI
+parameter SLTIU = 6'b001011; //SLTIU
+parameter ORI	= 6'b001101; //ORI
+parameter XORI  = 6'b001110; //XORI
+parameter LW	= 6'b100011; //LW
+parameter SW	= 6'b101011; //SW
+parameter LB	= 6'b100000; //LB
+parameter SB	= 6'b101000; //SB
+parameter LBU	= 6'b100100; //LBU
+parameter BEQ	= 6'b000100; //BEQ
+parameter BNE	= 6'b000101; //BNE
+parameter BGTZ	= 6'b000111; //BGTZ
 
 parameter RTYPE = 000000; //R-Type INSN
 
@@ -106,12 +103,192 @@ begin : DECODE
 					sa_out = insn[10:6];
 					func_out = insn[5:0];
 				end
+
+				ADDU: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[10:6];
+					func_out = insn[5:0];
+				end
+
+				SUB: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[10:6];
+					func_out = insn[5:0];
+				end
+
+				SUBU: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[10:6];
+					func_out = insn[5:0];
+				end
 	
 				MULT: begin
 					opcode_out = RTYPE;
 					rs_out = insn[25:21];
 					rt_out = insn[20:16];
 					rd_out = 00000;
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				MULTU: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = 00000;
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				DIV: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = 00000;
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				DIVU: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = 00000;
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				MFHI: begin
+					opcode_out = RTYPE;
+					rs_out = 00000;
+					rt_out = 00000;
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				MFLO: begin
+					opcode_out = RTYPE;
+					rs_out = 00000;
+					rt_out = 00000;
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				SLT: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				SLTU: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				SLL: begin
+					opcode_out = RTYPE;
+					rs_out = 00000;
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[15:6];
+					func_out = insn[5:0];
+				end
+
+				SLLV: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				SRL: begin
+					opcode_out = RTYPE;
+					rs_out = 00000;
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[10:6];
+					func_out = insn[5:0];
+				end
+
+				SRLV: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				SRA: begin
+					opcode_out = RTYPE;
+					rs_out = 00000;
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = insn[10:6];
+					func_out = insn[5:0];
+				end
+
+				SRAV: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				AND: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				OR: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				NOR: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
+					sa_out = 00000;
+					func_out = insn[5:0];
+				end
+
+				JALR: begin
+					opcode_out = RTYPE;
+					rs_out = insn[25:21];
+					rt_out = insn[20:16];
+					rd_out = insn[15:11];
 					sa_out = 00000;
 					func_out = insn[5:0];
 				end
